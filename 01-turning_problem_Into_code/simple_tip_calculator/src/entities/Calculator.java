a89b9f189d3a836fe3d2b117bb1b5596f0c00af2
naneraruom <naneraruom@gmail.com>
2020-12-02 17:06:56 -0300

    estrutura

---
+++ 01-turning_problem_Into_code/simple_tip_calculator/src/entities/Calculator.java
@@ -0,0 +1,16 @@
+package entities;
+
+public class Calculator {
+
+    double billAmount;
+    double tipRate;
+
+    public double tip() {
+        return 1D;
+    }
+
+    public double total() {
+        return 1D;
+    }
+
+}
